eca7356fb3e97e55b82ec6f6b1f2edb4760c6225
Ruchira Moitra <31311577+ruchiramoitra@users.noreply.github.com>
2018-05-10 10:15:59 +0530

    3rd commit

---
+++ exitgeo.php
@@ -0,0 +1,19 @@
+<?php
+$con = mysqli_connect("localhost", "id2263671_sherlock", "sherlock", "id2263671_sherlock");
+
+$traffic_id = $_POST["traffic_id"]
+
+$statement = mysqli_prepare($con, "DELETE FROM ambulance WHERE traffic_id="1");
+mysqli_stmt_execute($statement);
+ $response = array();
+    $response["success"] = false; 
+ while(mysqli_stmt_fetch($statement)){
+      
+  $response["success"] = true;
+   }
+    
+    
+echo json_encode($response);
+
+echo json_encode(array("result"=>$response));
+?>
