9bebd3c38d96ca8c43be34af27cf0bf1483e1bb9
Ruchira Moitra <31311577+ruchiramoitra@users.noreply.github.com>
2018-05-10 10:15:25 +0530

    Add files via upload

---
+++ traffic.php
@@ -0,0 +1,32 @@
+<?php
+   
+ $con = mysqli_connect("localhost", "id2263671_sherlock", "sherlock", "id2263671_sherlock");
+   
+ 
+ 
+    $traffic_id = $_POST["traffic_id"];
+  
+  $source = $_POST["source"];
+  
+  $destination = $_POST["destination"];
+  
+  $current = $_POST["current"];
+    
+   
+ 
+   
+   
+    $statement = mysqli_prepare($con, "INSERT INTO ambulance (traffic_id,source,destination,current) VALUES (?, ?, ?,?)");
+  
+  mysqli_stmt_bind_param($statement, "ssss",$traffic_id,$source,$deistination,$current);
+   
+ mysqli_stmt_execute($statement);
+  
+  
+    $response = array();
+ 
+   $response["success"] = true;  
+    
+ 
+   echo json_encode($response);
+?>
